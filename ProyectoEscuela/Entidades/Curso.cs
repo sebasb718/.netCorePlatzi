e1aa000550d787a67fbd7803f68e210cbdfbbe64
sebasb718 <sebasb718@gmail.com>
2019-08-13 10:47:03 -0500

    terminado curso de fundamentos, incluyo comparacion entre foreach y linq(se demora mas)

--- ProyectoEscuela/Entidades/Curso.cs
+++ ProyectoEscuela/Entidades/Curso.cs
@@ -10,6 +10,7 @@ namespace ProyectoEscuela.Entidades
         public TiposJornada Jornada { get; set; }
         public List<Asignatura> Asignaturas{ get; set; }
         public List<Alumno> Alumnos{ get; set; }
+        public List<Evaluaciones> Evaluaciones{ get; set; }
         public Curso() => UniqueID = Guid.NewGuid().ToString();
         
     }
